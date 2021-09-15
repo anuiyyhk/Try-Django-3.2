e736bab2746d311a316adc4973ff870418e4fb62
anuiyyhk <90723873+anuiyyhk@users.noreply.github.com>
2021-09-15 14:24:21 +0100

    Update settings.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -26,7 +26,7 @@ DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
-# if not DEBUG:
+if not DEBUG:
 ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
 
 # Application definition
